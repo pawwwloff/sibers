d676e281ba44ad8068ae4d283b58504471fac2fb
pawwwloff <36720299+pawwwloff@users.noreply.github.com>
2018-03-13 17:08:22 +0700

    Update connectMysqli.php

--- core/connectMysqli.php
+++ core/connectMysqli.php
@@ -5,9 +5,9 @@
 namespace core;
 class connectMysqli extends \mysqli {
 	private $DBHost = "localhost";
-	private $DBLogin = "root";
-	private $DBPassword = "qwer1234";
-	private $DBName = "tz";
+	private $DBLogin = "****";
+	private $DBPassword = "****";
+	private $DBName = "****";
 	public function __construct() {
 		parent::__construct($this->DBHost, $this->DBLogin, $this->DBPassword, $this->DBName);
 		if (mysqli_connect_error()) {
